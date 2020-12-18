87ec6751c80189bc00d23bb102973c889c550b0c
David Sabalete <dsabalete@gmail.com>
2020-12-18 23:29:25 +0100

    add more example tweets

--- src/assets/users.js
+++ src/assets/users.js
@@ -9,13 +9,40 @@ export const users = [
         tweets: [
             {
                 id: 1,
-                content: 'Vue-twitter is amazing',
+                content:
+                    'The truth is often what we make of it; you heard what you wanted to hear, believed what you wanted to believe.'
             },
             {
                 id: 2,
-                content: 'Do not forget to subscribe',
+                content:
+                    'Luke, you’re going to find that many of the truths we cling to depend greatly on our own point of view.'
             },
-        ],
+            {
+                id: 3,
+                content: 'Be mindful of your thoughts, Anakin, they betray you.'
+            },
+            {
+                id: 4,
+                content: 'So uncivilized.'
+            },
+            {
+                id: 5,
+                content: 'Only a Sith deals in absolutes.'
+            },
+            {
+                id: 6,
+                content:
+                    'If you strike me down, I shall become more powerful than you can possibly imagine.'
+            },
+            {
+                id: 7,
+                content: 'I have the high ground.'
+            },
+            {
+                id: 8,
+                content: 'Use the Force, Luke.'
+            }
+        ]
     },
     {
         id: 2,
@@ -24,7 +51,54 @@ export const users = [
         lastName: 'Skywalker',
         email: null,
         isAdmin: false,
-        tweets: [],
+        tweets: [
+            {
+                id: 1,
+                content:
+                    'But I was going into Tosche Station to pick up some power converters!'
+            },
+            {
+                id: 2,
+                content:
+                    'If there’s a bright center to the universe, you’re on the planet that it’s farthest from.'
+            },
+            {
+                id: 3,
+                content:
+                    'I want to come with you to Alderaan. There’s nothing for me here now. I want to learn the ways of the Force, and become a Jedi like my father.'
+            },
+            {
+                id: 4,
+                content:
+                    'That armor’s too strong for blasters. Rogue Group, use your harpoons and tow cables. Go for the legs. It might be our only chance of stopping them.'
+            },
+            {
+                id: 5,
+                content: 'You want the impossible.'
+            },
+            {
+                id: 6,
+                content: 'You’ll find I’m full of surprises.'
+            },
+            {
+                id: 7,
+                content:
+                    'I’m taking Captain Solo and his friends. You can either profit by this…or be destroyed.'
+            },
+            {
+                id: 8,
+                content:
+                    'You’ve failed, Your Highness. I am a Jedi, like my father before me.'
+            },
+            {
+                id: 9,
+                content: 'Impressive — every word in that sentence was wrong.'
+            },
+            {
+                id: 10,
+                content: 'No one’s ever really gone.'
+            }
+        ]
     },
     {
         id: 3,
@@ -33,7 +107,51 @@ export const users = [
         lastName: 'Solo',
         email: null,
         isAdmin: false,
-        tweets: [],
+        tweets: [
+            {
+                id: 1,
+                content:
+                    'It’s the ship that made the Kessel Run in less than twelve parsecs! I’ve outrun Imperial starships, not the local bulk-cruisers, mind you. I’m talking about the big Corellian ships now. She’s fast enough for you, old man.'
+            },
+            {
+                id: 2,
+                content: 'Over my dead body.'
+            },
+            {
+                id: 3,
+                content: 'Don’t everybody thank me at once.'
+            },
+            {
+                id: 4,
+                content: 'It’s not wise to upset a Wookiee.'
+            },
+            {
+                id: 5,
+                content:
+                    'Hokey religions and ancient weapons are no match for a good blaster at your side, kid.'
+            },
+            {
+                id: 6,
+                content: 'You know, sometimes I amaze even myself.'
+            },
+            {
+                id: 7,
+                content: 'Never tell me the odds!'
+            },
+            {
+                id: 8,
+                content: 'Let’s keep a little optimism here.'
+            },
+            {
+                id: 9,
+                content: 'Chewie, we’re home.'
+            },
+            {
+                id: 10,
+                content:
+                    'I used to wonder about that myself. Thought it was a bunch of mumbo-jumbo. A magical power holding together good and evil, the dark side and the light? Crazy thing is, it’s true. The Force, the Jedi — all of it. It’s all true.'
+            }
+        ]
     },
     {
         id: 4,
@@ -42,6 +160,58 @@ export const users = [
         lastName: 'Vader',
         email: null,
         isAdmin: false,
-        tweets: [],
-    },
+        tweets: [
+            {
+                id: 1,
+                content:
+                    'I am altering the deal. Pray I don’t alter it any further.”'
+            },
+            {
+                id: 2,
+                content:
+                    'I hope so for your sake, Commander. The Emperor is not as forgiving as I am.'
+            },
+            {
+                id: 3,
+                content:
+                    'Just for once let me look on you with my own eyes. You were right. You were right about me. Tell your sister you were right.'
+            },
+            {
+                id: 4,
+                content: 'That name no longer has any meaning for me'
+            },
+            {
+                id: 5,
+                content: 'This will be a day long remembered.'
+            },
+            {
+                id: 6,
+                content: 'All I am surrounded by is fear, and dead men.'
+            },
+            {
+                id: 7,
+                content: 'All too easy.'
+            },
+            {
+                id: 8,
+                content: 'Anakin Skywalker was weak. I destroyed him.'
+            },
+            {
+                id: 9,
+                content: 'Apology accepted, Captain Needa.'
+            },
+            {
+                id: 10,
+                content: 'Asteroids do not concern me, Admiral.'
+            },
+            {
+                id: 11,
+                content: 'Be careful not to choke on your aspirations.'
+            },
+            {
+                id: 12,
+                content: 'I find your lack of faith disturbing.'
+            }
+        ]
+    }
 ]
