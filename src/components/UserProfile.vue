e811b9714816d5c9ee6007897e65c95a00859f9b
David Sabalete Rodríguez <david.sabalete@lovetoknow.com>
2020-12-18 12:51:08 +0100

    add new tweet form

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -8,6 +8,33 @@
             <div class="user-profile__follower-count">
                 <strong>Followers: {{ followers }}</strong>
             </div>
+            <form
+                class="user-profile__create-tweet"
+                @submit.prevent="createNewTweet"
+            >
+                <label for="newTweet">
+                    <strong>New Tweet</strong>
+                </label>
+                <textarea
+                    id="newTweet"
+                    rows="4"
+                    v-model="newTweetContent"
+                ></textarea>
+
+                <div class="user-profile__create-tweet-type">
+                    <label for="netTweetType"><strong>Type</strong></label>
+                    <select id="newTweetType" v-model="selectedTweetType">
+                        <option
+                            :value="option.value"
+                            v-for="(option, index) in tweetTypes"
+                            :key="index"
+                            >{{ option.name }}</option
+                        >
+                    </select>
+                </div>
+
+                <button>Tweet!</button>
+            </form>
         </div>
         <div class="user-profile__tweets-wrapper">
             <TweetItem
@@ -31,6 +58,12 @@ export default {
     },
     data() {
         return {
+            newTweetContent: '',
+            selectedTweetType: 'instant',
+            tweetTypes: [
+                { value: 'draft', name: 'Draft' },
+                { value: 'instant', name: 'Instant Tweet' },
+            ],
             followers: 0,
             user: {
                 id: 1,
@@ -71,6 +104,16 @@ export default {
         toggleFavourite(id) {
             console.log(`Favourite Tweet ${id}`)
         },
+        createNewTweet() {
+            console.log(this.selectedTweetType)
+            if (this.newTweetContent && this.selectedTweetType !== 'draft') {
+                this.user.tweets.unshift({
+                    id: this.user.tweets.length + 1,
+                    content: this.newTweetContent,
+                })
+                this.newTweetContent = ''
+            }
+        },
     },
     mounted() {
         this.followUser()
@@ -82,14 +125,13 @@ export default {
 .user-profile {
     display: grid;
     grid-template-columns: 1fr 3fr;
-    width: 100%;
+    grid-gap: 50px;
     padding: 50px 5%;
 }
 
 .user-profile__user-panel {
     display: flex;
     flex-direction: column;
-    margin-right: 50px;
     padding: 20px;
     background-color: white;
     border-radius: 5px;
@@ -107,5 +149,16 @@ export default {
 
 h1 {
     margin: 0;
+}
+
+.user-profile__tweet-wrapper {
+    display: grid;
+    grid-gap: 10px;
+}
+
+.user-profile__create-tweet {
+    padding-top: 20px;
+    display: flex;
+    flex-direction: column;
 }
 </style>
