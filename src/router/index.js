1d30afb16586b2fdb1b3f94931018cc1101acc82
David Sabalete <dsabalete@gmail.com>
2020-12-18 21:01:44 +0100

    add more routing rules

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import UserProfile from '@/views/UserProfile.vue'
+import Admin from '@/views/Admin.vue'
 
 const routes = [
     {
@@ -13,11 +14,30 @@ const routes = [
         name: 'UserProfile',
         component: UserProfile,
     },
+    {
+        path: '/admin',
+        name: 'Admin',
+        component: Admin,
+        meta: {
+            requiresAdmin: true,
+        },
+    },
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+})
+
+router.beforeEach(async (to, from, next) => {
+    const isAdmin = true
+    const requiresAdmin = to.matched.some((record) => record.meta.requiresAdmin)
+
+    if (requiresAdmin && !isAdmin) {
+        next({ name: 'Home' })
+    } else {
+        next()
+    }
 })
 
 export default router
